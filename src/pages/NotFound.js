65e7b80a12d1dead9460587f53663728318ba7d7
Farah <f.deshnawy@aucegypt.edu>
2022-12-21 10:13:50 +0200

    router

---
+++ src/pages/NotFound.js
@@ -0,0 +1,8 @@
+const NotFound=()=>{
+    return(
+        <div>
+            <p>Page Not Founnd !</p>
+        </div>
+    )
+}
+export default NotFound;
---
+++ src/pages/QuoteDetails.js
@@ -0,0 +1,25 @@
+import { Route, useParams } from "react-router-dom";
+import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+const DUMMY_QUOTES = [
+    { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+    { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
+  ];
+  
+const QuoteDetails =()=>{
+   const params= useParams();
+   const quote=DUMMY_QUOTES.find( quote=> quote.id === params.quoteId );
+
+   if(!quote){
+    return <p>No quote found</p>
+   }
+    return (
+        <> 
+       <HighlightedQuote text={quote.text} author={quote.author} />
+        <Route path={`/quotes/${params.quoteId}/comments`}>
+            <Comments/>
+        </Route>
+        </>
+    )
+}
+export default QuoteDetails;
